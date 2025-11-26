7547a0336849d56cffdd994fef868cb2e3fb3c10
zulfalsa <zulfa2805@gmail.com>
2025-11-26 12:02:53 +0700

    repair #1

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      // Forces all libraries to use the single copy of React in your root node_modules
+      react: path.resolve(process.cwd(), 'node_modules/react'),
+      'react-dom': path.resolve(process.cwd(), 'node_modules/react-dom'),
+    },
+  },
   plugins: [
     react(),
     tailwindcss(),
